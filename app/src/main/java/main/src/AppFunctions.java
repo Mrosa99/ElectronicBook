e4fb0d63cd6b0bbac88d16b3e05ed6a1977b6a21
Mrosa99 <125097380+Mrosa99@users.noreply.github.com>
2023-09-29 14:31:49 -0400

    Final files
    Added the final files

---
+++ app/src/main/java/main/src/AppFunctions.java
@@ -0,0 +1,227 @@
+package main.src;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.sql.SQLException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class AppFunctions extends LogPage {
+    static ImageIcon book;
+
+    /**
+     * Sets the general style of a button by adding ActionListener, set Font,
+     * setFocusable, and the background color.
+     * 
+     * @param Buttons
+     */
+    void setStyle(JButton[] buttons) {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].addActionListener(this);
+            buttons[i].setFont(myFont);
+            buttons[i].setFocusable(false);
+            buttons[i].setBackground(Color.white);
+        }
+    }
+
+    /**
+     * Adds the Jbutton array into the a panel
+     * 
+     * @param panel
+     * @param buttons
+     */
+    void addButton(JPanel panel, JButton[] buttons) {
+        for (int i = 0; i < buttons.length; i++) {
+            panel.add(buttons[i]);
+            panel.repaint();
+        }
+    }
+
+    /**
+     * Checks to see if JButton text is empty(" "), if it is, it will remove
+     * ActionListener and make button "invsible". The button will not be visible in
+     * the frame.
+     * 
+     * @param buttons
+     */
+    void isEmpty(JButton[] buttons) {
+        for (int i = 0; i < buttons.length; i++) {
+            if (buttons[i].getText().equals(" ")) {
+                buttons[i].removeActionListener(this);
+                buttons[i].setOpaque(false);
+                buttons[i].setContentAreaFilled(false);
+                buttons[i].setBorderPainted(false);
+                buttons[i].setIcon(null);;
+            }
+        }
+    }
+
+    /**
+     * Resets empty buttons to original style.
+     * 
+     * @param buttons
+     */
+    void reset(JButton[] buttons) {
+        for (int i = 0; i < buttons.length; i++) {
+            if (buttons[i].getText().equals(" ")) {
+                buttons[i].addActionListener(this);
+                buttons[i].setOpaque(true);
+                buttons[i].setContentAreaFilled(true);
+                buttons[i].setBorderPainted(true);
+            }
+        }
+    }
+
+    /**
+     * 
+     * Sets the Button with no text
+     * 
+     * @param button
+     */
+    void setButtons(JButton[] button) {
+        for (int i = 0; i < button.length; i++) {
+            button[i] = new JButton();
+        }
+    }
+
+    /**
+     * 
+     * Removes JButtons from a given JPanel.
+     * 
+     * @param panel
+     * @param Added_Buttons
+     */
+    void removeButtons(JPanel panel, JButton[] Added_Buttons) {
+        for (int i = 0; i < Added_Buttons.length; i++) {
+            panel.remove(Added_Buttons[i]);
+            panel.repaint();
+        }
+    }
+
+    /**
+     * 
+     * Gets and sets the buttons for the BookStore and the Library. Uses count and
+     * pgmax to switch through each page
+     * 
+     * @param count
+     * @param pgmax
+     * @param buttons
+     * @param tmpBooks
+     * @throws SQLException
+     */
+    void getAvailableTitles(int count, int pgmax, JButton[] buttons, String[] tmpBooks, String bookID)
+            throws SQLException {
+        for (int i = 0; i < buttons.length; i++) {
+            tmpBooks[i] = " ";
+        }
+        String LibQuery = "SELECT * FROM Library limit " + count + "," + pgmax + "";
+        Connect.resultset = Connect.statement.executeQuery(LibQuery);
+        for (int i = 0; i < buttons.length; i++, count++) {
+            if (Connect.resultset.next()) {
+                tmpBooks[i] = Connect.resultset.getString("Title");
+                bookID = Connect.resultset.getString("BookID");
+                String bookImg = "EBook/ebook/src/images/" + bookID + ".jpeg";
+                book = new ImageIcon(bookImg);
+                Image image = book.getImage();
+                Image newImage = image.getScaledInstance(150, 162, java.awt.Image.SCALE_SMOOTH);
+                book = new ImageIcon(newImage);
+                buttons[i].setIcon(book);
+                buttons[i].setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            buttons[i].setText(tmpBooks[i]);
+        }
+    }
+
+    void getOwnedTitles(int count, int pgmax, JButton[] buttons, String[] tmpBooks, String bookID) throws SQLException {
+        for (int i = 0; i < buttons.length; i++) {
+            tmpBooks[i] = " ";
+        }
+        String ownedQuery = "SELECT * FROM Library INNER JOIN Orders ON Orders.BookID = Library.BookID WHERE UserID = ? LIMIT ?,?";
+        Connect.statement = Connect.c.prepareStatement(ownedQuery);
+        Connect.statement.setString(1, userID);
+        Connect.statement.setInt(2, count);
+        Connect.statement.setInt(3, pgmax);
+        Connect.resultset = Connect.statement.executeQuery();
+        for (int i = 0; i < buttons.length; i++, count++) {
+            if (Connect.resultset.next()) {
+                tmpBooks[i] = Connect.resultset.getString("Title");
+                bookID = Connect.resultset.getString("BookID");
+                String bookImg = "EBook/ebook/src/images/" + bookID + ".jpeg";
+                book = new ImageIcon(bookImg);
+                Image image = book.getImage();
+                Image newImage = image.getScaledInstance(150, 162, java.awt.Image.SCALE_SMOOTH);
+                book = new ImageIcon(newImage);
+                buttons[i].setIcon(book);
+            }
+            buttons[i].setText(tmpBooks[i]);
+        }
+    }
+
+    void getLink() {
+        try {
+            String ownedQuery = "SELECT  FROM Library INNER JOIN Orders ON Orders.BookID = Library.BookID WHERE UserID = ? LIMIT ?,?";
+            Connect.statement = Connect.c.prepareStatement(ownedQuery);
+            Connect.statement.setString(1, userID);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    void firstSet(JButton[] buttons1, JButton[] buttons2) {
+        buttons1[0] = new JButton("Back");
+        buttons1[1] = new JButton("Main Menu");
+        buttons1[2] = new JButton("Next");
+        setStyle(buttons1);
+        setButtons(buttons2);
+        setStyle(buttons2);
+    }
+
+    public String change(String change, String current, JLabel text, JLabel msg, String word) throws SQLException {
+        try {
+            Connect.query = "Select UserID from Users where binary " + word + "=?";
+            Connect.statement = Connect.c.prepareStatement(Connect.query);
+            Connect.statement.setString(1, change);
+            Connect.resultset = Connect.statement.executeQuery();
+            if (change.equals(current)) {
+                msg.setText("New " + word + " cannot be the same");
+                text.setText("Please try again:");
+            } else if (change.isEmpty() || change.isBlank()) {
+                msg.setText(word + " cannot be blank");
+                text.setText("Please try again");
+            } else if (Connect.resultset.next()) {
+                if (word == "Username") {
+                    msg.setText(word + " already exist");
+                    text.setText("Please try again:");
+                }
+            } else {
+                Connect.query = "UPDATE Users SET " + word + " = '" + change + "' WHERE " + word + "= '" + current
+                        + "'";
+                Connect.statement = Connect.c.prepareStatement(Connect.query);
+                Connect.statement.executeUpdate();
+                msg.setText(word + " Changed");
+                text.setText("<HTML><CENTER>Username successfully changed to: <BR>" + change + "<CENTER><HTML>");
+                current = change;
+            }
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+        return current;
+    }
+
+    void removeAction(JButton[] buttons) {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].removeActionListener(this);
+        }
+    }
+
+    void addAction(JButton[] buttons) {
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].addActionListener(this);
+        }
+    }
+
+}
---
+++ app/src/main/java/main/src/Settings.java
@@ -0,0 +1,249 @@
+package main.src;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Settings extends MainFrames {
+
+    static JFrame confirmFrame;
+    static JLabel settingsText;
+    static JLabel settings_msg;
+    static JPanel settingsCenter;
+    static JPanel settingsBottom;
+    protected JTextField tmpNewUser;
+    protected JTextField tmpNewPass;
+    protected static String newUsername = null;
+    protected static String newPassword = null;
+
+    static protected JButton[] Settings_Buttons = new JButton[3];
+    static protected JButton[] main = new JButton[1];
+
+    static private JButton[] confirmButtons = new JButton[2];
+
+    void SettingsFrame() {
+        Header_Message.setText("Settings");
+        Center_Panel.setLayout(new GridBagLayout());
+        Bottom_Panel.setLayout(new GridLayout(1, 3, 20, 0));
+
+        addButton(Center_Panel, Settings_Buttons);
+        addButton(Bottom_Panel, main);
+
+    }
+
+    void setSettButtons() throws SQLException {
+        if (Settings_Buttons[0] == null) {
+            Settings_Buttons[0] = new JButton("Change Username");
+            Settings_Buttons[1] = new JButton("Change Password");
+            Settings_Buttons[2] = new JButton("Delete Account");
+            main[0] = new JButton("Main Menu");
+            setStyle(main);
+            setStyle(Settings_Buttons);
+        }
+        SettingsFrame();
+    }
+
+    void confirm_frame() {
+        confirmFrame = new JFrame("E-Book Reader Settings");
+        confirmFrame.setIconImage(EIcon.getImage());
+        confirmFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        confirmFrame.setSize(425, 300);
+        confirmFrame.setLocationRelativeTo(null);
+        confirmFrame.setResizable(false);
+
+        settings_msg = new JLabel();
+        settings_msg.setText("Username Change");
+        settings_msg.setFont(myFont);
+
+        JPanel settingsTop = new JPanel();
+        settingsTop.setBackground(Color.white);
+        settingsTop.setPreferredSize(new Dimension(75, 50));
+        settingsTop.add(settings_msg);
+
+        settingsText = new JLabel();
+        settingsText.setText("Confirm to change username.");
+        settingsText.setFont(myFont);
+        settingsText.setPreferredSize(new Dimension(675, 350));
+        settingsText.setLayout(new FlowLayout());
+        settingsText.setBackground(Color.white);
+        settingsText.setOpaque(true);
+
+        settingsCenter = new JPanel();
+        settingsCenter.setBackground(Color.white);
+        settingsCenter.setPreferredSize(new Dimension(50, 50));
+        settingsCenter.setLayout(new GridBagLayout());// maybe change
+        settingsCenter.add(settingsText);
+
+        settingsBottom = new JPanel();
+        settingsBottom.setBackground(Color.white);
+        settingsBottom.setPreferredSize(new Dimension(50, 60));
+        settingsBottom.setLayout(new GridLayout(1, 2, 10, 0));
+        addButton(settingsBottom, confirmButtons);
+
+        confirmFrame.add(settingsTop, BorderLayout.NORTH);
+        confirmFrame.add(settingsCenter, BorderLayout.CENTER);
+        confirmFrame.add(settingsBottom, BorderLayout.SOUTH);
+
+        confirmFrame.setVisible(true);
+    }
+
+    void confirmButtons() {
+        if (confirmButtons[0] == null) {
+            confirmButtons[0] = new JButton("Not Now");
+            confirmButtons[1] = new JButton("Confirm");
+            setStyle(confirmButtons);
+        }
+        confirm_frame();
+    }
+
+    void new_name() {
+        settingsCenter.setLayout(new GridLayout(0, 1));
+        settingsText.setText("Please enter your new username:");
+
+        settingsText.setHorizontalAlignment(JLabel.CENTER);
+        tmpNewUser = new JTextField();
+        tmpNewUser.setPreferredSize(new Dimension(250, 40));
+        settingsCenter.add(tmpNewUser);
+
+    }
+
+    void userChange() throws SQLException {
+        newUsername = tmpNewUser.getText();
+        username = change(newUsername, username, settingsText, settings_msg, "Username");
+        String finish = settings_msg.getText();
+        if (finish.equals("Username Changed")) {
+            settingsCenter.remove(tmpNewUser);
+            settingsBottom.remove(confirmButtons[1]);
+            confirmButtons[0].setText("Close Window");
+        }
+    }
+
+    void new_password() {
+        settingsCenter.setLayout(new GridLayout(0, 1));
+        settingsText.setText("Please enter your new password:");
+
+        settingsText.setHorizontalAlignment(JLabel.CENTER);
+        tmpNewPass = new JTextField();
+        tmpNewPass.setPreferredSize(new Dimension(250, 40));
+        settingsCenter.add(tmpNewPass);
+
+    }
+
+    void passChange() throws SQLException {
+        newPassword = tmpNewPass.getText();
+        password = change(newPassword, password, settingsText, settings_msg, "Password");
+        String finish = settings_msg.getText();
+        if (finish.equals("Password Changed")) {
+            settingsCenter.remove(tmpNewPass);
+            settingsBottom.remove(confirmButtons[1]);
+            confirmButtons[0].setText("Close Window");
+        }
+    }
+
+    void deleteUser() throws SQLException {
+        settingsText.setText("Account Deleted");
+        Connect.query = "DELETE FROM `Users` WHERE `Users`.`UserID` = ?";
+        Connect.statement = Connect.c.prepareStatement(Connect.query);
+        Connect.statement.setString(1, userID);
+        Connect.statement.executeUpdate();
+        settingsBottom.remove(confirmButtons[1]);
+        confirmButtons[0].setText("Close App");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == main[0]) {
+            Header_Message.setText("Main Menu");
+            removeButtons(Center_Panel, Settings_Buttons);
+            removeButtons(Bottom_Panel, main);
+            Center_Panel.setLayout(new GridLayout(2, 3, 10, 10));
+            addButton(Center_Panel, MainMenu_Buttons);
+
+        } else if (e.getSource() == Settings_Buttons[0]) {
+             removeAction(main);
+            removeAction(Settings_Buttons);
+            confirmButtons();
+            confirmFrame.setTitle("Username Change");
+        } else if (e.getSource() == Settings_Buttons[1]) {
+             removeAction(main);
+            removeAction(Settings_Buttons);
+            confirmButtons();
+            confirmFrame.setTitle("Password Change");
+            settingsText.setText("Confirm to change password.");
+            settings_msg.setText("Password Change");
+
+        } else if (e.getSource() == Settings_Buttons[2]) {
+            removeAction(main);
+            removeAction(Settings_Buttons);
+            confirmButtons();
+            confirmFrame.setTitle("Delete Account");
+            settingsText.setText("Confirm to delete account.");
+            settings_msg.setText("Delete Account");
+        } else if (e.getSource() == confirmButtons[0]) {
+            String tmp = confirmButtons[0].getText();
+            if (tmp == "Close App") {
+                confirmFrame.dispose();
+                MainFrame.dispose();
+                try {
+                    Connect.c.close();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            } else {
+                addAction(Settings_Buttons);
+                addAction(main);
+                confirmButtons[0].setText("Not Now");
+                confirmButtons[1].setText("Confirm");
+                confirmFrame.dispose();
+            }
+        } else if (e.getSource() == confirmButtons[1]) {
+            String Sname = confirmButtons[1].getText();
+            String msg = confirmFrame.getTitle();
+            if (msg == "Username Change") {
+                if (Sname == "Confirm") {
+                    new_name();
+                    confirmButtons[1].setText("Next");
+                } else if (Sname == "Next") {
+                    try {
+                        userChange();
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+            if (msg == "Password Change") {
+                if (Sname == "Confirm") {
+                    new_password();
+                    confirmButtons[1].setText("Next");
+                } else if (Sname == "Next") {
+                    try {
+                        passChange();
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+            if (msg == "Delete Account") {
+                if (Sname == "Confirm") {
+                    try {
+                        deleteUser();
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+}
